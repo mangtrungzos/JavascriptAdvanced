7f46858260cf185db6e5d0685fdbf76d32b9b86c
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-09-08 12:06:15 +0000

    8/9

--- IIFE/index.js
+++ IIFE/index.js
@@ -1,9 +1,10 @@
 // IIFE - Immediately Invoked Function Expression
 // Một hàm được tạo ra từ một biểu thức
 // Self - Invokeing Function
+// Trong mỗi lần gọi hàm ()() tạo ra 1 môi trường mới
 
 // IIFE - Syntax (func normal & arrow func)
-(function (){
+(function () {
     console.log("IIFE");
 })();
 
@@ -14,11 +15,9 @@
 // Have to ";" before function expression
 let fullName = "TrongSeag"; // Cause it will generate an err that is not the function
 
-(() => {
+(() => {})();
 
-})();
-
-// ex. 
+// ex.
 // let functionName = "TrongSeag" // here
 
 // (() => {})();
@@ -31,7 +30,8 @@ let i = 0;
     i++;
     console.log(i);
 
-    if(i < 10) // If i < 10, it will call myFunc()
+    if (i < 10)
+        // If i < 10, it will call myFunc()
         myFunc();
 })();
 
@@ -40,7 +40,7 @@ let i = 0;
 // Tuy nhiên đoạn mã ở trong đó biến và hàm nó không chui phạm vi Global và ảnh hưởng đến dự án
 
 // IIFE đảm bảo tính toàn vẹn dữ liệu (Dữ liệu Private)
-const app = (function() {
+const app = (function () {
     // Private
     const cars = [];
 
@@ -53,12 +53,12 @@ const app = (function() {
         },
         delete(index) {
             cars.splice(index, 1);
-        }
-    }
+        },
+    };
 })();
 
 // ex 1.
-const number = (function(a, b) {
+const number = (function (a, b) {
     a + b;
 })(1, 2);
 console.log(number); // Output: undefined
@@ -77,7 +77,7 @@ console.log(number); // Output: undefined
     function j() {
         return x ** x;
     }
-})(3)
+})(3);
 // Output: ? 81
 
 // Hàm js() được thực thi ngay lập tức sau khi được khai báo và được gọi là IIFE. Lưu ý rằng tham số x của hàm js được truyền thực tế với giá trị 3.
@@ -88,12 +88,41 @@ console.log(number); // Output: undefined
 
 // y(s()) có nghĩa là y(27) trả về 27 * 3 = 81.
 
-
 // When we call a(1):
 
 // x ban đầu được truyền vào là 1.
-// Câu lệnh x++ sẽ tăng x lên 2. Tuy nhiên, do toán tử ++ được sử dụng sau biến (x++ là hậu tố), 
+// Câu lệnh x++ sẽ tăng x lên 2. Tuy nhiên, do toán tử ++ được sử dụng sau biến (x++ là hậu tố),
 // Giá trị của x chỉ tăng sau khi được sử dụng. Nhưng vì câu lệnh này không sử dụng giá trị của x, nên chỉ có việc tăng x lên 2.
 // Hàm a trả về một hàm (closure) mà bên trong nó, biến x hiện đang có giá trị là 2.
 
+// ex.
+function a(x) {
+    x++;
+    return function () {
+        console.log(++x);
+    };
+}
 
+a(1)();
+a(1)();
+a(1)();
+
+// 1. a(1)();
+// Gọi a(1):
+// Giá trị ban đầu của x là 1.
+// Sau đó, x++ làm tăng x lên 2.
+// Hàm a(1) trả về hàm bên trong (closure), và giá trị của x lúc này là 2 trong môi trường cục bộ của hàm.
+// Gọi hàm bên trong:
+// ++x làm tăng x từ 2 lên 3.
+// In ra 3.
+
+// 2. a(1)();
+// Lần này, khi a(1) được gọi lại, một môi trường hoàn toàn mới được tạo ra:
+
+// Ban đầu, x lại là 1 trong môi trường mới.
+// Sau đó, x++ làm tăng x lên 2.
+// Hàm trả về hàm bên trong với x có giá trị là 2.
+// Gọi hàm bên trong:
+
+// ++x làm tăng x từ 2 lên 3.
+// In ra 3.
--- index.js
+++ index.js
@@ -1,95 +1,11 @@
-/**  Global scope  */
-var message = "Global scope"; // variable declared in global scope (var, let, const)
-// therefore we can access it from anywhere
+// Closure = Tính khép kín or bao đóng
+//           *A function can remember where it was created
+//           *and can access variables outside its scope
 
-function logger() {
-  console.log(message);
-}
+//           Ứng dụng tính private trong OOP
+//           Biến được tham chiếu (refer) trong closure sẽ không được xóa khỏi bộ nhớ khi hàm parent thực thi xong
 
-logger();
-
-/**  Code Block Scope  */
-{
-  const age = 22;
-}
-// console.log(age); // Error: age is not defined
-// if else, for, while, v.v in couple {} actually it's a code block
-
-// var: Sẽ nhảy ra phạm vị gần nhất ở bên ngoài
-
-/**  Local Scope   */
-// This Scope will be created when a function called
-
-function log() {
-  const fullName = "Johan";
-  function log2() {
-    console.log(fullName);
-  }
-  log2();
-}
-
-log();
-
-// functions can access variable outside their scope
-const ages = 23;
-
-function logAge() {
-  console.log(ages);
-}
-logAge();
-
-// Or :
-const names = "Sang";
-function logName() {
-  function showName() {
-    console.log(names);
-  }
-  showName();
-}
-logName();
-
-/**   Cách thức một biến được truy cập   */
-
-// Nó sẽ tìm biến ở phạm vi gần nhất để có thể truy cập
-// Mỗi biến sẽ được khai báo ở phạm vi khác nhau
-const agez = 18;
-
-{
-  const agez = 19;
-  {
-    {
-      const agez = 20;
-      {
-        const agez = 21;
-        console.log(agez);
-      }
-    }
-  }
-}
-
-// case 1: Khai báo sau khi được sử dụng
-// const agess = 18;
-// {
-//   {
-//     console.log(agess); // Error: can not access 'age' before initialization
-//     const agess = 19;
-//   }
-// }
-
-/**  Delete from memory */
-
-// Biến trong code block & trong hàm
-// Khi hàm thực thi xong sẽ xóa những biến thuộc phạm vi của hàm khỏi memory
-
-function logg() {
-  const rand = Math.random();
-  console.log(rand);
-}
-
-logg();
-
-//  Biến trong hàm được tham chiếu bởi 1 hàm
-function makeCounter() {
+function createdCounter() {
   let counter = 0;
 
   function increase() {
@@ -99,7 +15,108 @@ function makeCounter() {
   return increase;
 }
 
-const counter1 = makeCounter();
-console.log(counter1());
-console.log(counter1());
-console.log(counter1());
+const logCounter = createdCounter();
+
+console.log(logCounter());
+console.log(logCounter());
+console.log(logCounter());
+
+//
+function createLogger(namespace) {
+  function logger(message) {
+    console.log(`[${namespace}] ${message}`);
+  }
+
+  return logger;
+}
+
+/** ---------- App ----------- */
+
+// Register.js
+const infoLogger = createLogger("Info");
+
+infoLogger("start send email");
+infoLogger("Send email fail, try again...");
+infoLogger("Send email success for user xxx");
+
+// Forgot password
+const errorLogger = createLogger("Error");
+
+errorLogger("email does not exist in DB");
+errorLogger("Send email fail, try again...");
+errorLogger("Send email success for user xxx");
+
+// Applying closure to resolve: Save DB into localStorage
+function createStorage(key) {
+  const store = JSON.parse(localStorage.getItem(key)) ?? {};
+
+  const save = () => {
+    localStorage.setItem(key, JSON.stringify(store));
+  };
+
+  const storage = {
+    get(key) {
+      return store[key];
+    },
+    set(key, value) {
+      store[key] = value; // store["fullName"] = "Sang";
+      save(); // store = { "fullName": "Sang" };
+    },
+    remove(key) {
+      delete store[key];
+      save();
+    },
+  };
+
+  return storage;
+}
+
+// Profile.js
+const profileSetting = createStorage("profile_setting");
+
+profileSetting.set("fullName", "Sang");
+profileSetting.set("age", 22);
+profileSetting.set("address", "VietNam");
+
+console.log(profileSetting.get("fullName"));
+// profileSetting.remove("fullName");
+
+// ex.
+function createApp() {
+  const cars = [];
+
+  return {
+    add(car) {
+      cars.push(car);
+    },
+    show() {
+      console.log(cars);
+    },
+  };
+}
+
+const app = createApp();
+app.add("BMW");
+app.show();
+
+// ex.
+function a(x) {
+  x++;
+  return function () {
+    console.log(++x);
+  };
+}
+
+// IIFE
+a(1)(); // x = 1, sau đó x++ => x = 2
+// return func ++x tăng x từ 2 lên 3
+a(1)(); // Kết quả này giống hệt lần đầu, Cause x chỉ tổn tại cục bộ trong mỗi lần gọi hàm (1 môi trường mới được tạo ra)
+a(1)();
+
+// Closure
+// hàm bên trong nhớ và giữ nguyên giá trị của biến x trong lần gọi đầu tiên.
+let x = a(1);
+x();
+x();
+x();
+// Output: 3,3,3 & 3,4,5
