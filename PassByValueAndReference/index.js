66e434400dd40da4d16fe27a7232f456c2d9e133
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-09-12 10:05:23 +0000

    12/9

---
+++ PassByValueAndReference/index.js
@@ -0,0 +1,10 @@
+// Pass by value = Truyền theo giá trị
+//   1. Sao chép giá trị của biến: let y = x
+//   2. Truyền bản sao vào bên trong hàm
+//   *Đặc điểm: Gán lại tham số trong hàm, không làm thay đổi biến bên ngoài hàm.
+
+// Pass by reference = Truyền theo tham chiếu (địa chỉ)
+//   Truyền trực tiếp tham chiếu của biến vào bên trong hàm.
+//   *Đặc điểm: Gán lại tham số trong hàm, ngay lập tức thay đổi biến bên ngoài hàm.
+
+// Note: JS chỉ có "Pass by value"
--- index.js
+++ index.js
@@ -1,119 +1,20 @@
-let a = 1; // Tạo ra biến a, cấp 1 ô nhớ và lưu giá trị của a vào ô nhớ
-
-let b = a; // Tạo ra biến b, cấp 1 ô nhớ khác, sao chép giá trị của a (1) vào ô nhớ
-
-a = 2; // Sửa giá trị trong ô nhớ của a thành 2
-
-// Biến a Giá trị = 2, Ô nhớ = 2
-// Biến b Giá trị = 1, Ô nhớ = 1
-
-console.log(b); // output = 1
-
-/** ----------- Reference types ----------- */
-// ex. 2
-let c = {
-  name: "Mercedes",
-};
-// Tạo ra biến c, cấp 1 ô nhớ và lưu giá trị {...} vào ô nhớ
-// trả về địa chỉ đã lưu và gán cho biến c
-// a: Giá trị: <#001> | Địa chỉ: #001 | Ô nhớ: { name: 'Mercedes' }
-
-let d = c;
-// Tạo ra biến d, trỏ biến d tới cùng địa chỉ ô nhớ của c
-// d: Giá trị: <#001> | Địa chỉ: #001
-
-c.name = "BMW";
-// Sửa giá trị trong ô nhớ của c thành "BMW"
-
-console.log(d.name);
-
-// ex. 2
-let a1 = {
-  name: "Mercedes",
-};
-// Tạo ra địa chỉ #001 lưu vào a
-// a: Giá trị: <#001> | Địa chỉ: #001 | Ô nhớ: { name: 'Mercedes' }
-
-a1 = {
-  name: "BMW",
-  model: "i8",
-};
-// Gán vào a 1 object mới & địa chỉ #002
-// a: Giá trị: <#002> | Địa chỉ: #002 | Ô nhớ: { name: 'BMW', model: 'i8' }
-
-// *Khi biến được gán = 1 object mới thì 1 vùng nhớ mới được tạo ra
-
-console.log(a1); // Output: { name: 'BMW', model: 'i8' }
-
-/** ----------- Case: Object containing child object ----------- */
-const student = {
-  name: "TrongSeag",
-  profile: {
-    firstName: "Trong",
-    lastName: "Seag",
-    introduction: "Boy",
-  },
-};
-
-const studentPRofile = student.profile;
-student.profile.introduction = "good";
-console.log(studentPRofile.introduction); // Output: good
-
-/** ------------ Data types with function ------------- */
-// Value types
-function sum(a, b) {
-  console.log(a, b);
-}
-
-const c1 = 1;
-const d1 = 2;
-sum(c1, d1);
-
-// Reference types
-function func(obj) {
-  // Copy địa chỉ của biến a vào obj
-  // Trỏ tới địa chỉ và sửa data trong ô nhớ thông qua địa chỉ đó
-  obj.name = "Mercedes";
-  console.log(obj);
-}
-
-const a2 = {
-  name: "BMW",
-};
-
-func(a2);
-console.log(a2);
-
-// Side effect (Tác dụng phụ)
-// ex.
-function createCar(obj) {
-  obj = JSON.parse(JSON.stringify(obj)); // Tạo ra 1 vùng nhớ mới
-  // or obj = {...obj} - Chỉ kéo 1 cấp key: value
-  obj.name = "Honda";
-  return obj;
-}
-
-const car = {
-  name: "Toyota",
-};
-
-const newCar = createCar(car);
-console.log(newCar);
-console.log(car);
+/**---------- Pass by value ------------ */
 
 // ex.
-const s = {
-  name: "BMW",
-};
+function changeValue(y) {
+  console.log(y);
+  y = 20;
+}
 
-const v = {
-  name: "BMW",
-};
+let x = 10;
+changeValue(x);
+console.log(x);
 
-console.log(s === v); // Ouput: false
-// ex.
-const s1 = {
-  name: "BMW",
-};
-const v1 = s1;
-console.log(s1 === v1);
+function changeValues(y) {
+  y = { name: "Bob" };
+  // y.name = "Bob";
+}
+
+let x1 = { name: "John" };
+changeValues(x1);
+console.log(x1);
