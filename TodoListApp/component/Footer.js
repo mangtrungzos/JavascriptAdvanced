d9b96cd3eee59e06b821705d3b5f97d07d67a277
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-09-27 15:31:24 +0000

    27/9

---
+++ TodoListApp/component/Footer.js
@@ -0,0 +1,24 @@
+import html from "../core.js";
+
+// In this situation: App is a component
+function Footer() {
+  return html`
+    <footer class="footer">
+      <span class="todo-count"><strong>0</strong> item left</span>
+      <ul class="filters">
+        <li>
+          <a class="selected" href="#/">All</a>
+        </li>
+        <li>
+          <a href="#/active">Active</a>
+        </li>
+        <li>
+          <a href="#/completed">Completed</a>
+        </li>
+      </ul>
+      <button class="clear-completed">Clear completed</button>
+    </footer>
+  `;
+}
+
+export default Footer;
